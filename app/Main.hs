29541320009e994d5e1ae4188c4b9688a77daec7
Luka Matitaishvili <LM01wili@gmail.com>
2017-05-29 19:03:20 +0400

    12345

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,21 @@ v1 = WithText "три"
 v2 = WithNumeral 3
 v3 = Empty
 
+
+
+
+addDistances (Distanceskm n1) + (Distanceskm n2) = Distanceskm (n1 + n2)
+
+addNamber (Namber n1) - (Namber n2) = Namber (n1 - n2)
+
+addYardage (Namber n1) * (Namber n2) = Namber (n1 * n2)
+
+
+
+
+
+
+
+
 main :: IO ()
 main = someFunc
