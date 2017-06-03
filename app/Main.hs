5d19804e2bdad06f6e6ddbc202665cc6dbe72313
Luka Matitaishvili <LM01wili@gmail.com>
2017-06-03 11:17:42 +0400

    124147

--- app/Main.hs
+++ app/Main.hs
@@ -23,5 +23,32 @@ addYardage (Namber n1) * (Namber n2) = Namber (n1 * n2)
 
 
 
+
+Num (Distanceskm n) = (Distanceskm n1) + (Distanceskm n2)
+Num (Distancesml n) = (Distancesml n1) + (Distancesml n2)
+
+
+Num (Distanceskm n) = (Distanceskm n1) * (Distanceskm n2)
+Num (Distancesml n) = (Distancesml n1) * (Distancesml n2)
+
+
+Num (Distanceskm n) abs = |(Distanceskm n)|
+Num (Distancesml n) abs = |(Distancesml n)|
+
+
+nam (n) fromInteger = (Distancesml n)
+nam (n) fromInteger = (Distanceskm n)
+
+
+Num (Distanceskm n) = (Distanceskm n1) - (Distanceskm n2)
+Num (Distancesml n) = (Distancesml n1) - (Distancesml n2)
+
+
+
+
+
+
+
+
 main :: IO ()
 main = someFunc
